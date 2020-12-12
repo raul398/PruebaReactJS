6503a56f145d0b9526784781c2df30151e5b1ec2
raul398 <raul398@hotmail.com.ar>
2020-12-12 18:24:24 +0100

    first commit

---
+++ fitness/src/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const name = 'Raul'
+const User = {
+  name : 'Raul',
+  lastname : 'Argañaraz Estay',
+  avatar : 'https://icons.iconarchive.com/icons/diversity-avatars/avatars/1024/batman-icon.png'
+}
+
+function getName(user){
+  return (user.name + ' ' + user.lastname)
+}
+
+function getGreeting(user){
+  if (user) {
+    return <h1>Hello {getName(User)}</h1>
+  } else {
+    return 'No hay Usuario'
+  }
+}
+
+//const element = <div>{getGreeting(User)}</div>
+
+const element = (
+  <div>
+    <h1>{getGreeting(User)}</h1>
+    <img src= {User.avatar} />
+  </div>
+)
+
+const container = document.getElementById('root')
+
+//ReactDOM.render(__QUEE LEMENTO, __DONDE LO VA A RENDERIAR)
+
+ReactDOM.render(element, container)
